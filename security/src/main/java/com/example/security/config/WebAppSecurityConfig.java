f7cb334a79dcc5c8ea7c81a2e8441ceec2443f17
qiunanya <1766226354@qq.com>
2020-03-20 17:54:26 +0800

    提交修改默认登录字段名，登录成功跳转，指定登录页

--- security/src/main/java/com/example/security/config/WebAppSecurityConfig.java
+++ security/src/main/java/com/example/security/config/WebAppSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +16,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebAppSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+
+        super.configure(builder);
+    }
+
+    @Override
     protected void configure(HttpSecurity security) throws Exception {
         /**
         * 描述: <br>重写父类方法 configure
@@ -24,7 +31,11 @@ public class WebAppSecurityConfig extends WebSecurityConfigurerAdapter {
         * .authorizeRequests()//对请求进行授权
         * .anyRequest() // 任意请求
         * .authenticated();// 需要登录以后才可以访问
-         * formLogin(); // 开启默认登录
+        * formLogin(); // 开启默认登录
+        * .loginPage("login.html") //设置登录页，提交表单action：login.html method="post"
+        * .loginProcessingUrl() //登录地址：一旦开启就会覆盖 loginPage默认值
+        * .defaultSuccessUrl() // 默认登录成功要跳转的页面路径
+        * .usernameParameter("userName").passwordParameter("passWord") // 修改默认登录表单属性字段名
         * @Author QiuKing
         * @Date 2020/3/19 17:11
         */
